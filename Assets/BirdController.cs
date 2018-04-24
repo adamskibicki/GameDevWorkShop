5f55dc16a75be3f7b7a4390084138e3b555a3549
AdamSkibicki <adam-skibicki@outlook.com>
2018-04-24 16:37:07 +0200

    Initial commit

---
+++ Assets/BirdController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2D;
+
+    private bool isDead;
+
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isDead)
+        {
+            rigidbody2D.velocity = new Vector2(0, 5);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        isDead = true;
+    }
+}
---
+++ Assets/ColumnsCOntroller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnsCOntroller : MonoBehaviour
+{
+
+    private Rigidbody2D rigidbody2D;
+
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        rigidbody2D.velocity = new Vector2(-2, 0);
+    }
+
+
+}
