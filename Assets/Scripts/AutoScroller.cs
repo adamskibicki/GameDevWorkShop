7cb20b9b2e7b87de744c4511ae0b12bf26bec44e
AdamSkibicki <adam-skibicki@outlook.com>
2018-05-28 17:16:28 +0200

    Changes made during third workshops

---
+++ Assets/Scripts/AutoScroller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoScroller : MonoBehaviour
+{
+    private Material material;
+
+    private void Awake()
+    {
+        material = GetComponent<MeshRenderer>().material;
+    }
+
+    private void Update()
+    {
+        material.mainTextureOffset = new Vector2(material.mainTextureOffset.x + Time.deltaTime * 0.1f, 0);
+    }
+}
